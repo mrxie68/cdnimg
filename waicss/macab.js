a511d6f4756951e978bf6e9fdb9284d9757928bc
mrxie68 <41711995+mrxie68@users.noreply.github.com>
2023-03-21 22:41:05 +0800

    Update macab.js

--- waicss/macab.js
+++ waicss/macab.js
@@ -1,1 +1,13 @@
+const svg = document.getElementById('mytop-svg');
+const container = document.getElementById('my-container');
+let clicked = false;
 
+svg.addEventListener('click', () => {
+  if (!clicked) {
+    container.scrollIntoView({ behavior: 'smooth', block: 'start' }); // 跳到容器前面
+    clicked = true;
+  } else {
+    container.scrollIntoView({ behavior: 'smooth', block: 'end' }); // 跳到容器后面
+    clicked = false;
+  }
+});
